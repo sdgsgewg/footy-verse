a419a6a37878d3816008b6729b8cf953465538fc
Jessen <jessen123ptk@gmail.com>
2026-08-04 00:42:34 +0700

    fix root navbar layout

--- src/components/layout/navbar/root/Navbar.tsx
+++ src/components/layout/navbar/root/Navbar.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
   return (
     <header>
       <nav className="border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60 sticky top-0 z-50">
-        <div className="flex h-16 items-center px-4 container mx-auto justify-between">
+        <div className="flex h-16 items-center px-4 lg:container lg:mx-auto justify-between">
           <div className="flex items-center">
             {/* Logo and Website Name */}
             <NavbarLogo />
