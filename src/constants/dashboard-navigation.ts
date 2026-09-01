6bc9691dc11aa17aa72cffdb3eb386a689a77770
Jessen <jessen123ptk@gmail.com>
2026-09-01 16:12:38 +0700

    fix dashboard navigation

--- src/constants/dashboard-navigation.ts
+++ src/constants/dashboard-navigation.ts
@@ -1,41 +1,41 @@
 import { Flag, Goal, Map, Network, Shield, Trophy, Users } from "lucide-react";
 
-import { ROUTES } from "@/constants/routes";
+import { ENTITY_CONFIG } from "@/config/entities";
 
 export const DASHBOARD_CONTENT_ITEMS = [
   {
     key: "players",
-    href: ROUTES.DASHBOARD.CONTENT.PLAYERS.BASE,
+    href: ENTITY_CONFIG["player"]["dashboardRoute"],
     icon: Users,
   },
   {
     key: "clubs",
-    href: ROUTES.DASHBOARD.CONTENT.CLUBS.BASE,
+    href: ENTITY_CONFIG["club"]["dashboardRoute"],
     icon: Shield,
   },
   {
     key: "nationalities",
-    href: ROUTES.DASHBOARD.CONTENT.NATIONALITIES.BASE,
+    href: ENTITY_CONFIG["nationality"]["dashboardRoute"],
     icon: Flag,
   },
   {
     key: "positions",
-    href: ROUTES.DASHBOARD.CONTENT.POSITIONS.BASE,
+    href: ENTITY_CONFIG["position"]["dashboardRoute"],
     icon: Goal,
   },
   {
     key: "regions",
-    href: ROUTES.DASHBOARD.CONTENT.REGIONS.BASE,
+    href: ENTITY_CONFIG["region"]["dashboardRoute"],
     icon: Map,
   },
   {
     key: "confederations",
-    href: ROUTES.DASHBOARD.CONTENT.CONFEDERATIONS.BASE,
+    href: ENTITY_CONFIG["confederation"]["dashboardRoute"],
     icon: Network,
   },
   {
     key: "competitions",
-    href: ROUTES.DASHBOARD.CONTENT.COMPETITIONS.BASE,
+    href: ENTITY_CONFIG["competition"]["dashboardRoute"],
     icon: Trophy,
   },
 ] as const;
