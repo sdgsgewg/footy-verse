f20b52c7d1db52bd0024b104fbf2ecce5bc6eca3
Jessen <jessen123ptk@gmail.com>
2026-09-14 14:58:56 +0700

    fix image optimizationn config

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,14 @@ const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 const nextConfig: NextConfig = {
   images: {
     formats: ["image/webp"],
+    minimumCacheTTL: 2678400, // 31 days
+    deviceSizes: [640, 768, 1024, 1440],
+    imageSizes: [32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "wmghprjdobvoxthkdlnh.supabase.co",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
